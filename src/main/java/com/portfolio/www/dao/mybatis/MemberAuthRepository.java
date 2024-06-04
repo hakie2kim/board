e8e8e45cd599aff20a9e4ad1eb88c489294fe9f5
Hakjun Kim <hakie2kim@gmail.com>
2024-06-04 13:23:15 +0900

    test: 회원 가입 테스트 추가
    - 가입한 아이디를 사용해 이미 존재하는 아이디인지 확인
    - 가입하지 않은 아이디를 사용해 이미 존재하는 아이디인지 확인
    - 회원 가입 - member, member_auth에 insert 여부 확인, join() 메서드의 리턴 값 Message 확인
    - 유효하지 않은 인증 이메일 URI인지 확인
    - 유효한 인증 이메일 URI인지 확인
    - 만료된 인증 이메일인지 확인

--- src/main/java/com/portfolio/www/dao/mybatis/MemberAuthRepository.java
+++ src/main/java/com/portfolio/www/dao/mybatis/MemberAuthRepository.java
@@ -5,6 +5,7 @@ import com.portfolio.www.dto.MemberAuthDto;
 public interface MemberAuthRepository {
 	public int addMemberAuth(MemberAuthDto memberAuthDto);
 	public MemberAuthDto findMemberAuth(String uri);
+	public MemberAuthDto findMemberAuthByMemberSeq(int memberSeq);
 	public String findAuthYn(int memberSeq);
 	public int updateEmailAuthYn(MemberAuthDto memberAuthDto);
 	public void updateMemberAuth(MemberAuthDto memberAuthDto);
